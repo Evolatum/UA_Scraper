ec741e0bb9358233ebba7e7256ad7c0e5017d585
Santiago MVO <santiago.mvao@gmail.com>
2020-01-19 19:09:51 -0600

    added mongo models

---
+++ models/Article.js
@@ -0,0 +1,26 @@
+// Dependencies
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Schema declaration
+var ArticleSchema = new Schema({
+  headline: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+});
+
+// Export the model
+module.exports = mongoose.model("Article", ArticleSchema);
---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+// Dependencies
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Schema declaration
+var CommentSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+// Export the model
+module.exports = mongoose.model("Note", CommentSchema);
