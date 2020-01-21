43e7016d695ccdd46ef704ba924afb58d025b033
Santiago MVO <santiago.mvao@gmail.com>
2020-01-21 12:28:37 -0600

    added button for deleting comment (missing ajax), styled comments

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,16 @@ $ref = {
   comments:$("#commentsSection"),
 }
 
+$(document).ready(function(){
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+});
+
 // On comments button click
 $(document).on("click", ".commentsBtn", function() {
   $ref.comments.empty();
@@ -16,7 +26,7 @@ $(document).on("click", ".commentsBtn", function() {
     console.log(data.comments);
     if(data.comments){
       for(let comment of data.comments){
-        $ref.comments.append(`<h4>${comment.title}</h4><p>${comment.text}</p>`);
+        $ref.comments.append(`<div class="comment"><h4>${comment.title}</h4><span class="delComment" id="${comment._id}" aria-hidden="true">&times;</span><p>${comment.text}</p></div>`);
       }
     }
 
@@ -54,3 +64,25 @@ $(document).on("click", ".addCommentBtn", function(event) {
       $("#commentText").val();
     });
 });
+/*
+// Delete comment
+$(document).on("click", ".addCommentBtn", function(event) {
+  event.preventDefault()
+  var thisId = $(this).attr("id").replace("comment-","");
+  var data= {
+    title: $("#commentTitle").val(),
+    text: $("#commentText").val()
+  }
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: data
+  })
+    .then(function(data) {
+      console.log(data);
+      $("#commentTitle").val();
+      $("#commentText").val();
+    });
+});
+*/
